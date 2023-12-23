4e8d15cdb35c425b1b47aad151908ebb8c10d523
4honeymoon <mkhutinaeva@bk.ru>
2023-12-23 04:04:08 +0300

    Initial commit

---
+++ src/Task2_Queue.java
@@ -0,0 +1,8 @@
+public interface Queue
+{
+    Object dequeue();
+    Object element();
+    void enqueue(Object o);
+    boolean isEmpty();
+    boolean clear();
+}
---
+++ src/Task3_Add.java
@@ -0,0 +1,10 @@
+class Add extends BinaryOperation {
+    public Add(Expression left, Expression right) {
+        super(left, right);
+    }
+
+    @Override
+    public int evaluate(int x) {
+        return left.evaluate(x) + right.evaluate(x);
+    }
+}
